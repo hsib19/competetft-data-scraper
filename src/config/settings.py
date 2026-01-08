545f7e294374f126a6ef109982ff85c0c2b36ef4
Hasib Muharam <hasibmuha@gmail.com>
2026-01-08 07:29:25 +0700

    chore: add config settings for base URL, headers, and directories

---
+++ src/config/settings.py
@@ -0,0 +1,6 @@
+BASE_URL = "https://competetft.com"
+HEADERS = {
+    "User-Agent": "CompetetFTScraper/1.0 (+https://github.com/hsib19/competetft-data-scraper)"
+}
+DATA_DIR = "data"
+LOG_DIR = "logs"
