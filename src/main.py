279167f14e78ec2ff170420ac3b552888e2a3c70
Hasib Muharam <hasibmuha@gmail.com>
2026-01-08 07:39:45 +0700

    feat(core): add async main.py with storage integration for all scraper modules

--- src/main.py
+++ src/main.py
@@ -1,19 +1,31 @@
+import asyncio
 from events.events_scraper import scrape as scrape_events
 from schedule.schedule_scraper import scrape as scrape_schedule
 from tournament.tournament_scraper import scrape as scrape_tournaments
 from tournament.participants_scraper import scrape as scrape_participants
 from pro_points.pro_points_scraper import scrape as scrape_pro_points
 from ladder_points.ladder_scraper import scrape as scrape_ladder_points
+from utils.logger import logger
 
-def main():
-    print("Starting CompetetFT Scraper...")
-    scrape_events()
-    scrape_schedule()
-    scrape_tournaments()
-    scrape_participants()
-    scrape_pro_points()
-    scrape_ladder_points()
-    print("Scraper finished.")
+async def main():
+    logger.info("Starting CompetetFT Scraper...")
+
+    # Run a;; scraper async
+    tasks = [
+        asyncio.to_thread(scrape_events),
+        asyncio.to_thread(scrape_schedule),
+        asyncio.to_thread(scrape_tournaments),
+        asyncio.to_thread(scrape_participants),
+        asyncio.to_thread(scrape_pro_points),
+        asyncio.to_thread(scrape_ladder_points)
+    ]
+
+    results = await asyncio.gather(*tasks)
+    
+    logger.info("All scraper modules finished.")
+    logger.info("Results summary:")
+    for res in results:
+        logger.info(f" - {len(res)} items scraped")
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
