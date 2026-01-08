1fb225f538b285fc68bdf08398b733b9d3602acd
Hasib Muharam <hasibmuha@gmail.com>
2026-01-08 07:30:07 +0700

    feat: add basic logging setup using loguru

---
+++ src/utils/logger.py
@@ -0,0 +1,6 @@
+from loguru import logger
+import os
+from config.settings import LOG_DIR
+
+os.makedirs(LOG_DIR, exist_ok=True)
+logger.add(f"{LOG_DIR}/scraper.log", rotation="1 MB", retention="7 days", level="INFO")
