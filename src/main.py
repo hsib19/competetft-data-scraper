2a8c3d679aea1f6e672d6b0bc411fee768062c14
Hasib Muharam <hasibmuha@gmail.com>
2026-01-08 07:37:10 +0700

    feat(core): integrate all scraper modules in main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-from utils.logger import logger
+from events.events_scraper import scrape as scrape_events
+from schedule.schedule_scraper import scrape as scrape_schedule
+from tournament.tournament_scraper import scrape as scrape_tournaments
+from tournament.participants_scraper import scrape as scrape_participants
+from pro_points.pro_points_scraper import scrape as scrape_pro_points
+from ladder_points.ladder_scraper import scrape as scrape_ladder_points
 
 def main():
-    logger.info("Starting CompetetFT Scraper...")
-    # TODO: call scraper modules here
-    logger.info("Scraper finished.")
+    print("Starting CompetetFT Scraper...")
+    scrape_events()
+    scrape_schedule()
+    scrape_tournaments()
+    scrape_participants()
+    scrape_pro_points()
+    scrape_ladder_points()
+    print("Scraper finished.")
 
 if __name__ == "__main__":
     main()
