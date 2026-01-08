c7360dc0a1d2d485dccda325b335155fdfd84b30
Hasib Muharam <hasibmuha@gmail.com>
2026-01-08 07:30:22 +0700

    feat: add main.py as entry point for scraper execution

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from utils.logger import logger
+
+def main():
+    logger.info("Starting CompetetFT Scraper...")
+    # TODO: call scraper modules here
+    logger.info("Scraper finished.")
+
+if __name__ == "__main__":
+    main()
